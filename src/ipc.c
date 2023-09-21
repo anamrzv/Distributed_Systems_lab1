c418d498f8aca344171a95f05faedb6de60d6799
anamrzv <ana.mrzv@mail.ru>
2023-09-21 23:57:23 +0300

    implement initializing full mesh of pipes

--- src/ipc.c
+++ src/ipc.c
@@ -5,31 +5,66 @@
 #include "include/ipc.h"
 #include "include/process.h"
 
+
 int start_parent(long children_num) {
-    struct parent_process* parent_process = malloc(sizeof(struct parent_process));
-    parent_process->pid = getpid();
-    parent_process->local_id = PARENT_ID;
+    long processes_num = children_num + 1;
 
-    struct child_process* children;
-    children = (struct child_process*) malloc(children_num * sizeof(struct child_process));
-    if (children == NULL) {
-        fprintf(stderr, "Memory allocation failed.\n");
-        return -1;
-    }
-    parent_process->children = children;
+    int pipe_write_ends[processes_num][processes_num];
+    int pipe_read_ends[processes_num][processes_num];
+    int fd[2]; // todo: check position
 
-    for (size_t i = 0; i < children_num; i++) {
-        pid_t child_pid = fork();
-        if (child_pid == -1) {
-            perror("fork() failed");
-            return -1;
-        } else {
-//            struct child_process* new_process = malloc(sizeof(struct child_process));
-//            new_process->pid = child_pid;
-//            new_process->local_id = get_process_id();
-            parent_process->children[i].pid = child_pid;
-            parent_process->children[i].local_id = get_process_id();
+    // open all pipes
+    for (size_t from = 0; from < children_num; from++) {
+        for (size_t to = 0; to < children_num; to++) {
+            if (from != to) {
+                if (pipe(fd) < 0) {
+                    fprintf(stderr, "Creation of pipe failed.\n"); // todo: close pipes
+                    return -1;
+                }
+                pipe_read_ends[from][to] = fd[0];
+                pipe_write_ends[from][to] = fd[1];
+            }
         }
     }
+
+    for (int i = 0; i < children_num; i++) {
+        pid_t child_pid = fork();
+        if (child_pid == -1) {
+            printf("fork() failed for process number %d\n", i + 1); // todo: close pipes
+            return -1;
+        } else if (child_pid > 0) { // we are in parent process
+            //leave only its ends
+            for (size_t from = 0; from < processes_num; from++) {
+                for (size_t to = 0; to < processes_num; to++) {
+                    if (from == to) continue;
+
+                    if (from != 0) {
+                        close(pipe_write_ends[from][to]);
+                    }
+                    if (to != 0) {
+                        close(pipe_read_ends[from][to]);
+                    }
+                }
+            }
+        } else if (child_pid == 0) { // we are in child process
+            //leave only its ends
+            for (size_t from = 0; from < processes_num; from++) {
+                for (size_t to = 0; to < processes_num; to++) {
+                    if (from == to) continue;
+
+                    int current_child_num = i + 1;
+                    if (from != current_child_num) {
+                        close(pipe_write_ends[from][to]);
+                    }
+                    if (to != current_child_num) {
+                        close(pipe_read_ends[from][to]);
+                    }
+                }
+            }
+
+        }
+    }
+
+
     return 0;
 }
--- src/process.c
+++ src/process.c
@@ -4,12 +4,12 @@
 
 #include "include/process.h"
 
-int process_id;
-
-int get_process_id() {
-    static int counter = 0;
-    if (process_id == 0) {
-        process_id = ++counter;
-    }
-    return process_id;
-}
+//int process_id;
+//
+//int get_process_id() {
+//    static int counter = 0;
+//    if (process_id == 0) {
+//        process_id = ++counter;
+//    }
+//    return process_id;
+//}
